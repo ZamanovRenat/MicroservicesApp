9f9c558bfc9b45748fb01e62c42f20de0882cb11
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-16 21:18:50 +0300

    Базовый интерфейс для сервисов

---
+++ MicroservicesApp.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using MicroservicesApp.Web.Models;
+
+namespace MicroservicesApp.Web.Services.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}

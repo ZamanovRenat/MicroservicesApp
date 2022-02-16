6049d97114391817941bcd414576cf530acddf29
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-16 20:51:07 +0300

    Контроллер товаров

---
+++ MicroservicesApp.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MicroservicesApp.Services.ProductAPI.Models.DTO;
+using MicroservicesApp.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MicroservicesApp.Services.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();
+        }
+        /// <summary>
+        /// Получение всех товаров
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        /// <summary>
+        /// Получение одного товара по Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        /// <summary>
+        /// Добавление товара
+        /// </summary>
+        /// <param name="productDto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<object> Post([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        /// <summary>
+        /// Редактирование товара
+        /// </summary>
+        /// <param name="productDto"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public async Task<object> Put([FromBody] ProductDto productDto)
+        {
+            try
+            {
+                ProductDto model = await _productRepository.CreateUpdateProduct(productDto);
+                _response.Result = model;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+        /// <summary>
+        /// Удаление товара
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                bool isSuccess = await _productRepository.DeleteProduct(id);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}

6364e9c9e93f642d642a677c7c02ff8c5ae1bc60
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-16 16:34:56 +0300

    Добавление Основного проекта

---
+++ MicroservicesApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MicroservicesApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

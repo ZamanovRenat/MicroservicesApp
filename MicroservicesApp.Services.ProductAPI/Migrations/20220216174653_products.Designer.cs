334cd9f4a6390693fdfbe787041bc1106da95486
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-16 20:51:34 +0300

    Миграция в БД товаров

--- MicroservicesApp.Services.ProductAPI/Migrations/20220216174653_products.Designer.cs
+++ MicroservicesApp.Services.ProductAPI/Migrations/20220216174653_products.Designer.cs
@@ -3,14 +3,16 @@ using MicroservicesApp.Services.ProductAPI.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MicroservicesApp.Services.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220216174653_products")]
+    partial class products
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

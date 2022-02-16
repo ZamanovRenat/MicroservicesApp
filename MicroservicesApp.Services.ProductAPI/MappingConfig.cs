b046bd627df26eb0c8982849ed3aea9a4e56f742
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-16 19:19:23 +0300

    Настройка автомаппера в классе MappingConfig

---
+++ MicroservicesApp.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MicroservicesApp.Services.ProductAPI.Models;
+using MicroservicesApp.Services.ProductAPI.Models.DTO;
+
+namespace MicroservicesApp.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}

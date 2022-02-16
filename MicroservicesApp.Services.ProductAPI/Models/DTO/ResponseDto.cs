16db73ba28ac4da3fb704b6d6999de779935b6fa
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-16 19:10:53 +0300

    DTO для ответа от системы

---
+++ MicroservicesApp.Services.ProductAPI/Models/DTO/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MicroservicesApp.Services.ProductAPI.Models.DTO
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}

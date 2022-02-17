aceb0df61b24976441fbe72971349bef868b2a63
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-17 11:33:04 +0300

    Добавление модели пользователя

--- MicroservicesApp.Services.Identity/Models/ApplicationUser.cs
+++ MicroservicesApp.Services.Identity/Models/ApplicationUser.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace MicroservicesApp.Services.Identity.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }

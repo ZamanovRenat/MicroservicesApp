2ae8d9e2d75aaf21d59a60ac265de1b9aa19911b
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-17 11:25:16 +0300

    Добавление проекта Identity

---
+++ MicroservicesApp.Services.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroservicesApp.Services.Identity.Models
+{
+    public class ApplicationUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}

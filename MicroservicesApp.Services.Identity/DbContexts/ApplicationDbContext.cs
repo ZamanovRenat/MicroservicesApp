e65f06259e30d4191304f3a34e3bed9e7cb2fb61
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-17 11:33:24 +0300

    Добавление класса контекста БД Идентити

---
+++ MicroservicesApp.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MicroservicesApp.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesApp.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}

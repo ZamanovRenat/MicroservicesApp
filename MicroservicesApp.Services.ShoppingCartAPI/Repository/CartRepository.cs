418a18df8a93654fa84673c9c49ddf0e21f04eaa
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-18 22:36:26 +0300

    Репозиторий БД API Корзины

---
+++ MicroservicesApp.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using MicroservicesApp.Services.ShoppingCartAPI.DbContexts;
+using MicroservicesApp.Services.ShoppingCartAPI.Models;
+using MicroservicesApp.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesApp.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private IMapper _mapper;
+
+        public CartRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+        /// <summary>
+        /// Получение деталей корзины для пользователя по Id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task<CartDto> GetCartByUserId(string userId)
+        {
+            Cart cart = new()
+            {
+                CartHeader = await _db.CartHeaders.FirstOrDefaultAsync(u => u.UserId == userId)
+            };
+            //Добавление в детали корзины товаров используя жадную загрузку
+            cart.CartDetails = _db.CartDetails
+                .Where(u => u.CartHeaderId == cart.CartHeader.CartHeaderId).Include(u => u.Product);
+
+            return _mapper.Map<CartDto>(cart);
+        }
+        /// <summary>
+        /// Универсальный метод добавления и обновления корзины
+        /// </summary>
+        /// <param name="cartDto"></param>
+        /// <returns></returns>
+        public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            Cart cart = _mapper.Map<Cart>(cartDto);
+
+            //Проверка наличии записи товара в локальной БД АПИ корзины, если ее нет то добавление в БД
+            var prodInDb = await _db.Products
+                .FirstOrDefaultAsync(u => u.ProductId == cartDto.CartDetails.FirstOrDefault()
+                .ProductId);
+            if (prodInDb == null)
+            {
+                _db.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+                await _db.SaveChangesAsync();
+            }
+
+
+            //Проверка, определен ли заголовок или нет
+            var cartHeaderFromDb = await _db.CartHeaders.AsNoTracking()
+                .FirstOrDefaultAsync(u => u.UserId == cart.CartHeader.UserId);
+
+            if (cartHeaderFromDb == null)
+            {
+                //Добавление заголовк и записей о товаре для корзины
+                _db.CartHeaders.Add(cart.CartHeader);
+                await _db.SaveChangesAsync();
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+                cart.CartDetails.FirstOrDefault().Product = null;
+                _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                await _db.SaveChangesAsync();
+            }
+            else
+            {
+                //Логика если заголовок был определен ранее
+                //содержат ли запись о товары детали корзины
+                var cartDetailsFromDb = await _db.CartDetails.AsNoTracking().FirstOrDefaultAsync(
+                    u => u.ProductId == cart.CartDetails.FirstOrDefault().ProductId &&
+                    u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+                
+                //Проверка определены ли детали корзины
+                if (cartDetailsFromDb == null)
+                {
+                    //определение деталей
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = cartHeaderFromDb.CartHeaderId;
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    _db.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+                }
+                else
+                {
+                    //обновление кол-ва товара в деталях корзины
+                    cart.CartDetails.FirstOrDefault().Product = null;
+                    cart.CartDetails.FirstOrDefault().Count += cartDetailsFromDb.Count;
+                    cart.CartDetails.FirstOrDefault().CartDetailsId = cartDetailsFromDb.CartDetailsId;
+                    cart.CartDetails.FirstOrDefault().CartHeaderId = cartDetailsFromDb.CartHeaderId;
+                    _db.CartDetails.Update(cart.CartDetails.FirstOrDefault());
+                    await _db.SaveChangesAsync();
+                }
+            }
+
+            return _mapper.Map<CartDto>(cart);
+        }
+        /// <summary>
+        /// Удаление из корзины
+        /// </summary>
+        /// <param name="cartDetailsId"></param>
+        /// <returns></returns>
+        public async Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            try
+            {
+                CartDetails cartDetails = await _db.CartDetails
+                    .FirstOrDefaultAsync(u => u.CartDetailsId == cartDetailsId);
+
+                int totalCountOfCartItems = _db.CartDetails
+                    .Where(u => u.CartHeaderId == cartDetails.CartHeaderId).Count();
+
+                _db.CartDetails.Remove(cartDetails);
+                if (totalCountOfCartItems == 1)
+                {
+                    var cartHeaderToRemove = await _db.CartHeaders
+                        .FirstOrDefaultAsync(u => u.CartHeaderId == cartDetails.CartHeaderId);
+
+                    _db.CartHeaders.Remove(cartHeaderToRemove);
+                }
+                await _db.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Очистка корзины
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task<bool> ClearCart(string userId)
+        {
+            var cartHeaderFromDb = await _db.CartHeaders.FirstOrDefaultAsync(u => u.UserId == userId);
+            if (cartHeaderFromDb != null)
+            {
+                _db.CartDetails
+                    .RemoveRange(_db.CartDetails.Where(u => u.CartHeaderId == cartHeaderFromDb.CartHeaderId));
+                _db.CartHeaders.Remove(cartHeaderFromDb);
+                await _db.SaveChangesAsync();
+                return true;
+
+            }
+            return false;
+        }
+    }
+}

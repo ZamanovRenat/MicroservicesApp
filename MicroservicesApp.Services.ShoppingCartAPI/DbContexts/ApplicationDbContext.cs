766ab2684a5d1ffbc8f64482d8b01e292c60c1af
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-18 15:59:03 +0300

    Класс контекста БД в API корзины

---
+++ MicroservicesApp.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesApp.Services.ShoppingCartAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}

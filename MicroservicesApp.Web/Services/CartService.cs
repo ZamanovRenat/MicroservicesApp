fdde8eb3bea4ef40dd8566e173fdab90c6d925d7
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-18 23:54:11 +0300

    Устранение ошибки

--- MicroservicesApp.Web/Services/CartService.cs
+++ MicroservicesApp.Web/Services/CartService.cs
@@ -29,7 +29,7 @@ namespace MicroservicesApp.Web.Services
             return await this.SendAsync<T>(new ApiRequest()
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.ProductAPIBase + "/api/cart/GetCart/" + userId,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/GetCart/" + userId,
                 AccessToken = token
             });
         }

550e5b059807eb001a345ab1765b701212e8e13d
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 15:51:27 +0300

    Добавление в контроллер корзины логики примения купонов и пересчета цены

--- MicroservicesApp.Web/Controllers/CartController.cs
+++ MicroservicesApp.Web/Controllers/CartController.cs
@@ -14,13 +14,16 @@ namespace MicroservicesApp.Web.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICartService _cartService;
+        private readonly ICouponService _couponService;
 
         public CartController(
             IProductService productService, 
-            ICartService cartService)
+            ICartService cartService,
+            ICouponService couponService)
         {
             _productService = productService;
             _cartService = cartService;
+            _couponService = couponService;
         }
         public async Task<IActionResult> CartIndex()
         {
@@ -84,10 +87,20 @@ namespace MicroservicesApp.Web.Controllers
 
             if (cartDto.CartHeader != null)
             {
+                if (!string.IsNullOrEmpty(cartDto.CartHeader.CouponCode))
+                {
+                    var coupon = await _couponService.GetCoupon<ResponseDto>(cartDto.CartHeader.CouponCode, accessToken);
+                    if (coupon != null && coupon.IsSuccess)
+                    {
+                        var couponObj = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(coupon.Result));
+                        cartDto.CartHeader.DiscountTotal = couponObj.DiscountAmount;
+                    }
+                }
                 foreach (var detail in cartDto.CartDetails)
                 {
                     cartDto.CartHeader.OrderTotal += (detail.Product.Price * detail.Count);
                 }
+                cartDto.CartHeader.OrderTotal -= cartDto.CartHeader.DiscountTotal;
             }
             return cartDto;
         }

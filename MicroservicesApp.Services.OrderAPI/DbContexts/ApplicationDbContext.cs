da0682dd1c6022c67efa1d8b6e70096cbddb11a3
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 23:27:51 +0300

    Добавление проекта OrderAPI и его моделей

---
+++ MicroservicesApp.Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MicroservicesApp.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesApp.Services.OrderAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+    }
+}

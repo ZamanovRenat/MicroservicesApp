8277b722d14b349b1cfe385c61a426114c04123f
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 13:06:41 +0300

    Класс контекста БД для API купонов

---
+++ MicroservicesApp.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesApp.Services.CouponAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}

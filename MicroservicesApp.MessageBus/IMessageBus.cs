373528a3f7f2891051161ad2f90c3bbd1d657f8a
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 20:16:35 +0300

    Бибилиотека классов сервисной шины передачи сообщений, базовый класс и интерфейс сообщений

---
+++ MicroservicesApp.MessageBus/IMessageBus.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MicroservicesApp.MessageBus
+{
+    public interface IMessageBus
+    {
+        Task PublishMessage(BaseMessage message, string topicName);
+    }
+}

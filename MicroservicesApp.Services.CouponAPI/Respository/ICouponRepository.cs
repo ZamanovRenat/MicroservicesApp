4c8e8ece38913859eacb285b7aa1ed3953e78ab6
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 13:58:44 +0300

    Контроллер купонов

--- MicroservicesApp.Services.CouponAPI/Respository/ICouponRepository.cs
+++ MicroservicesApp.Services.CouponAPI/Respository/ICouponRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace MicroservicesApp.Services.CouponAPI.Respository
+﻿using System.Threading.Tasks;
+using MicroservicesApp.Services.CouponAPI.Models.Dto;
+
+namespace MicroservicesApp.Services.CouponAPI.Respository
 {
     public interface ICouponRepository
     {
-        
+        Task<CouponDto> GetCouponByCode(string couponCode);
     }
 }

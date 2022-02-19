9ace7dde833d28454550fbabf6e357eb30dfb1a8
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 19:05:57 +0300

    Исправление ошибки вывода скидки на страницу

---
+++ MicroservicesApp.Services.CouponAPI/Migrations/20220219154835_GetCouponByCode.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MicroservicesApp.Services.CouponAPI.Migrations
+{
+    public partial class GetCouponByCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

6001bf3ac5858ba6024c040844ea46d887c63ee1
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 13:29:20 +0300

    DbSet для купонов в БД

--- MicroservicesApp.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
+++ MicroservicesApp.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MicroservicesApp.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MicroservicesApp.Services.CouponAPI.DbContexts
 {
@@ -8,5 +9,6 @@ namespace MicroservicesApp.Services.CouponAPI.DbContexts
         {
 
         }
+        public DbSet<Coupon> Coupons { get; set; }
     }
 }

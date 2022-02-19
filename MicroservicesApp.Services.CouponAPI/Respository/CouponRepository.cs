9a1c96988603c25b979958f64c71f480b5fbc72f
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-19 13:47:48 +0300

    Реализация репозитория купонов

---
+++ MicroservicesApp.Services.CouponAPI/Respository/CouponRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using MicroservicesApp.Services.CouponAPI.DbContexts;
+using MicroservicesApp.Services.CouponAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesApp.Services.CouponAPI.Respository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly ApplicationDbContext _db;
+        protected IMapper _mapper;
+        public CouponRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<CouponDto> GetCouponByCode(string couponCode)
+        {
+            var couponFromDb = await _db.Coupons.FirstOrDefaultAsync(u => u.CouponCode == couponCode);
+            return _mapper.Map<CouponDto>(couponFromDb);
+        }
+    }
+}

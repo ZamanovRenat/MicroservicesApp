a60068436427c60c7765749fdd20900431086384
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-20 12:35:05 +0300

    Настройка приема сообщений и сохранение Заказов в базе данных. Строка подключения, наименование подписки, топика

--- MicroservicesApp.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
+++ MicroservicesApp.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,20 +1,71 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using MicroservicesApp.Services.OrderAPI.Messages;
+using MicroservicesApp.Services.OrderAPI.Models;
+using MicroservicesApp.Services.OrderAPI.Repository;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace MicroservicesApp.Services.OrderAPI.Messaging
 {
     public class AzureServiceBusConsumer
     {
+        private readonly OrderRepository _orderRepository;
+        private readonly IConfiguration _configuration;
+        private readonly string serviceBusConnectionString;
+        private readonly string subscriptionName;
+        private readonly string checkoutMessageTopic;
+        public AzureServiceBusConsumer(OrderRepository orderRepository, IConfiguration configuration)
+        {
+            _orderRepository = orderRepository;
+            _configuration = configuration;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+            subscriptionName = _configuration.GetValue<string>("SubscriptionName");
+            checkoutMessageTopic = _configuration.GetValue<string>("CheckoutMessageTopic");
+        }
         private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
         {
             var message = args.Message;
             var body = Encoding.UTF8.GetString(message.Body);
 
             CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
-
+            
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDto.UserId,
+                FirstName = checkoutHeaderDto.FirstName,
+                LastName = checkoutHeaderDto.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CardNumber = checkoutHeaderDto.CardNumber,
+                CouponCode = checkoutHeaderDto.CouponCode,
+                CVV = checkoutHeaderDto.CVV,
+                DiscountTotal = checkoutHeaderDto.DiscountTotal,
+                Email = checkoutHeaderDto.Email,
+                ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDto.OrderTotal,
+                PaymentStatus = false,
+                Phone = checkoutHeaderDto.Phone,
+                PickupDateTime = checkoutHeaderDto.PickupDateTime
+            };
+            foreach (var detailList in checkoutHeaderDto.CartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detailList.ProductId,
+                    ProductName = detailList.Product.Name,
+                    Price = detailList.Product.Price,
+                    Count = detailList.Count
+                };
+                orderHeader.CartTotalItems += detailList.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+            //Добавление в БД
+            await _orderRepository.AddOrder(orderHeader);
         }
     }
 }

cec028693bb0f85da79d04996fc72d7b9cb443ff
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-20 11:05:10 +0300

    Интерфейс репозитория API заказов

---
+++ MicroservicesApp.Services.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using MicroservicesApp.Services.OrderAPI.Models;
+
+namespace MicroservicesApp.Services.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+        Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+    }
+}

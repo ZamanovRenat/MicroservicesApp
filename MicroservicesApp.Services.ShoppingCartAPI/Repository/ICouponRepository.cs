632654e6913beecee4e4cf48757c06d31572a993
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-20 15:01:17 +0300

    Репозиторий купонов в API корзины

---
+++ MicroservicesApp.Services.ShoppingCartAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using MicroservicesApp.Services.ShoppingCartAPI.Models.Dto;
+
+namespace MicroservicesApp.Services.ShoppingCartAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCoupon(string couponName);
+    }
+}

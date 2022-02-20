3f7bf6dfc264c1c4c1d794c1e99207c4f7d34d28
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-20 20:21:54 +0300

    Миграция в БД сервиса Логов Емайл

--- MicroservicesApp.Services.Email/Repository/EmailRepository.cs
+++ MicroservicesApp.Services.Email/Repository/EmailRepository.cs
@@ -19,6 +19,28 @@ namespace MicroservicesApp.Services.Email.Repository
         public async Task SendAndLogEmail(UpdatePaymentResultMessage message)
         {
             //implement an email sender or call some other class library
+            /*
+             * public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+ 
+            emailMessage.From.Add(new MailboxAddress("Inst", "some@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("Inst", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+ 
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                await client.AuthenticateAsync("*********@yandex.ru", System.Environment.GetEnvironmentVariable("*********"));
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+             */
             EmailLog emailLog = new EmailLog()
             {
                 Email = message.Email,

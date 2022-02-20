80f78d130662a056e9f0c3b090e1c698e20dff19
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-20 11:54:06 +0300

    Класс получения сообщений с AzureServiceBusConsumer

---
+++ MicroservicesApp.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using MicroservicesApp.Services.OrderAPI.Messages;
+using Newtonsoft.Json;
+
+namespace MicroservicesApp.Services.OrderAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private async Task OnCheckOutMessageReceived(ProcessMessageEventArgs args)
+        {
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);
+
+            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+
+        }
+    }
+}

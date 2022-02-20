0239cd6fa1aed20ea16e79784b4855233e21fa7f
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-20 19:45:25 +0300

    Репозиторий БД в сервисе Емейл

---
+++ MicroservicesApp.Services.Email/Repository/EmailRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using MicroservicesApp.Services.Email.DbContexts;
+using MicroservicesApp.Services.Email.Messages;
+using MicroservicesApp.Services.Email.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesApp.Services.Email.Repository
+{
+    public class EmailRepository : IEmailRepository
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _dbContext;
+
+        public EmailRepository(DbContextOptions<ApplicationDbContext> dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task SendAndLogEmail(UpdatePaymentResultMessage message)
+        {
+            //implement an email sender or call some other class library
+            EmailLog emailLog = new EmailLog()
+            {
+                Email = message.Email,
+                EmailSent = DateTime.Now,
+                Log = $"Order - {message.OrderId} has been created successfully."
+            };
+
+            await using var _db = new ApplicationDbContext(_dbContext);
+            _db.EmailLogs.Add(emailLog);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ MicroservicesApp.Services.Email/Repository/IEmailRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using MicroservicesApp.Services.Email.Messages;
+
+namespace MicroservicesApp.Services.Email.Repository
+{
+    public interface IEmailRepository
+    {
+        Task SendAndLogEmail(UpdatePaymentResultMessage message);
+    }
+}

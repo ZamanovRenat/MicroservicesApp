b99872f8b6220d8d91f2bbf3c6a3ebe51e9d2189
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-21 14:50:14 +0300

    Добавление класса отправки сообщений брокера сообщений RabbitMq в сервисе корзины

---
+++ MicroservicesApp.Services.ShoppingCartAPI/RabbitMQSender/IRabbitMQCartMessageSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MicroservicesApp.MessageBus;
+
+namespace MicroservicesApp.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public interface IRabbitMQCartMessageSender
+    {
+        void SendMessage(BaseMessage baseMessage, String queueName);
+    }
+}
---
+++ MicroservicesApp.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroservicesApp.MessageBus;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace MicroservicesApp.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
+    {
+        private readonly string _hostname;
+        private readonly string _password;
+        private readonly string _username;
+        private IConnection _connection;
+
+        public RabbitMQCartMessageSender()
+        {
+            _hostname = "localhost";
+            _password = "guest";
+            _username = "guest";
+        }
+        public void SendMessage(BaseMessage baseMessage, string queueName)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostname,
+                UserName = _username,
+                Password = _password
+            };
+            _connection = factory.CreateConnection();
+
+            using var channel = _connection.CreateModel();
+            channel.QueueDeclare(queue: queueName, false, false, false, arguments: null);
+            var json = JsonConvert.SerializeObject(baseMessage);
+            var body = Encoding.UTF8.GetBytes(json);
+            channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+        }
+    }
+}
